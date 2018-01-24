ec0105771507bb06c623659e39ad749ca696a8d9
Jose <jmriego@telefonica.net>
2018-01-24 16:57:34 +0000

    Refactored code for hammerspoon

--- hammerspoon.lua
+++ hammerspoon.lua
@@ -1,21 +1,39 @@
-mission_control = nil
---detect when mission control is opening with just a short countdown
-e = nil
-t = nil
-point_click = nil -- point in current screen to click on
-app = nil
+spaces = require("hs._asm.undocumented.spaces")
 
+--detect when mission control is opening with just a short countdown
+point_click = nil -- point in current screen to click on
+
+-- timer that tells whether screen is animating and I can't do anything else
+animating = nil
+animating = hs.timer.new(0.4, function() animating:stop(); end)
+
+found_window = nil
+all_spaces = nil
+visible_spaces = nil
+
+-- special tasks needed after switching to certain apps
+
+-- click on the lower bottom corner off the Gmail window to focus and allow hotkeys
+function after_gmail()
+    gmail_frame = hs.window.focusedWindow():frame()
+    point_click = {x=gmail_frame["x"]+15.0, y=gmail_frame["y"]+gmail_frame["h"]-15.0}
+end
+
+-- go to tab with Jira or open a new one
+function after_jira()
+    hs.execute('/usr/local/bin/chrome-cli activate -t $(/usr/local/bin/chrome-cli list links | grep "filter=50263" | grep -o "[0-9][0-9]*\\]" | head -1) || /usr/local/bin/chrome-cli open "https://bugs.indeed.com/issues/?filter=50263"')
+end
 
 -- keyboard keys to be used in mission control and the program/window to open
 -- the name of the app should be the names found in Applications folder
 key_params = {
-    -- ["g"] = {"Google Chrome", "Google Hangouts", false};
-    -- ["h"] = {"Google Chrome", "Google Hangouts", true};
+    -- ["g"] = {["app"]="Google Chrome", ["win"]="Google Hangouts", ["inclusive"]=false};
+    -- ["h"] = {["app"]="Google Chrome", ["win"]="Google Hangouts", ["inclusive"]=true};
     ["i"] = "Google Chrome";
-    ["j"] = "Google Chrome"; -- actually it will go to the jira tab or open it
+    ["j"] = {["app"] = "Google Chrome", ["after"]=after_jira};
     ["h"] = "YakYak";
     ["c"] = "Google Calendar";
-    ["g"] = "Gmail";
+    ["g"] = {["app"] = "Gmail", ["after"]=after_gmail};
     ["n"] = "Notes";
     ["e"] = "TextEdit";
     ["f"] = "Finder";
@@ -31,58 +49,90 @@ key_params = {
 }
 
 
-function mouseClick(button, point)
+function mouseClick(button, point, modifiers)
     local button = button or "left"
     local point = point or hs.mouse.getAbsolutePosition()
+    local modifiers = modifiers
     print(button .. '@' .. point.x .. ' , ' .. point.y)
     local clickState = hs.eventtap.event.properties.mouseEventClickState
-    hs.eventtap.event.newMouseEvent(hs.eventtap.event.types[(button .. "MouseDown")], point):setProperty(clickState, 1):post()
+    hs.eventtap.event.newMouseEvent(hs.eventtap.event.types[(button .. "MouseDown")], point, modifiers):setProperty(clickState, 1):post()
     hs.timer.doAfter(0.1, function() hs.eventtap.event.newMouseEvent(hs.eventtap.event.types[(button .. "MouseUp")], point):setProperty(clickState, 1):post(); end)
 end
 
-function launchOrFocus(app_win_inc)
-    local found_window = nil
+function moveCenterScreen(screen)
+    local rect = screen:fullFrame()
+    local center = hs.geometry.rectMidPoint(rect)
+    hs.mouse.setAbsolutePosition(center)
+end
 
-    if type(app_win_inc) == 'string'
-    then
-        app = app_win_inc
-        win = ''
-        inclusive = true
-    else
-        print(app_win_inc)
-        app = app_win_inc[1]
-        win = app_win_inc[2]
-        inclusive = app_win_inc[3]
+function index_of_space(sp)
+    local layout = spaces.layout()
+    for screen, spaces in pairs(layout) do
+        position = index_of(spaces, sp)
+        if position then
+            return position
+        end
     end
+    return false
+end
+
+function active_spaces()
+    local active_list = spaces.query(spaces.masks.currentSpaces)
+    local spaces_layout = spaces.layout()
+    local result = {}
+    for i, active_space in ipairs(active_list) do
+        for uuid, spaces in pairs(spaces_layout) do
+            if has_value(spaces, active_space) then
+                result[uuid] = active_space
+            end
+        end
+    end
+    return result
+end
+
+function launchOrFocus(app, win, inclusive)
+    local win = win or ''
+    local inclusive = inclusive==nil and true or inclusive
 
     local all_windows = hs.window.filter.new(true)
 
-    for k, window in pairs(all_windows:getWindows()) do
+    for dummy, window in pairs(all_windows:getWindows()) do
         if window:application():name() == app
         then
             local title = window:title()
             if title:len() > 0 and (title:find(win)==nil) ~= inclusive
             then
                 found_window = window
+                local found_window_space = found_window:spaces()[1] -- can it be in several spaces?
+                if not has_value(visible_spaces, found_window_space) then
+                    local found_window_space_pos = index_of_space(found_window_space)
+                    local visible_space_pos = index_of_space(visible_spaces[found_window:screen():spacesUUID()])
+                    local pt = hs.mouse.getAbsolutePosition()
+                    moveCenterScreen(window:screen())
+
+                    if found_window_space_pos > visible_space_pos then
+                        for i = visible_space_pos,found_window_space_pos,1 do
+                            hs.eventtap.keyStroke({'ctrl'}, "right")
+                            animating:start()
+                        end
+                    elseif found_window_space_pos < visible_space_pos then
+                        for i = visible_space_pos,found_window_space_pos,-1 do
+                            hs.eventtap.keyStroke({'ctrl'}, "left")
+                            animating:start()
+                        end
+                    end
+
+                    hs.mouse.setAbsolutePosition(pt)
+                end
                 found_window:application():activate(false)
-                hs.timer.doAfter(0, function() hs.execute('open ' .. found_window:application():path()); end)
                 return
             end
         end
     end
 
-    hs.execute('open ' .. hs.application.find(app):path())
+    hs.application.launchOrFocus(app)
 end
 
-
-function getCurrentWindowTitle()
-    local success, title = pcall(function() return hs.uielement.focusedElement():title(); end)
-    if not success
-    then
-       title = nil
-    end
-    return title
-end
 
 -- is the value in the table?
 function has_value(tab, val)
@@ -171,15 +221,6 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", function()
 end)
 
 
--- This is the main code for checking the key presses while mission control is open
--- it also detects left mouse button pressed and in that key it does nothing. whatever you pressed will be proccessed normally
-e = hs.eventtap.new({hs.eventtap.event.types.leftMouseDown}, function(event)
-    e:stop()
-    hs.timer.doAfter(0.1, function() k:exit(); end)
-    return false
-end)
-e:stop()
-
 -- ---------------
 -- bind shortcuts
 -- ---------------
@@ -188,16 +229,16 @@ e:stop()
 hs.hotkey.bind({"cmd"}, "pagedown", function() hs.spotify.next() end)
 hs.hotkey.bind({"cmd"}, "pageup", function() hs.spotify.previous() end)
 
-event_mm = hs.eventtap.new({hs.eventtap.event.types.otherMouseDown}, function(event)
+event_mmb = hs.eventtap.new({hs.eventtap.event.types.otherMouseDown}, function(event)
     mouse_button_pressed = event:getProperty(hs.eventtap.event.properties.mouseEventButtonNumber)
     if mouse_button_pressed == 2 then
         local ptMouse = hs.mouse.getAbsolutePosition()
-        event_mm:stop()
-        hs.timer.doAfter(0.0, function() mouseClick("middle", ptMouse); event_mm:start(); end)
+        event_mmb:stop()
+        hs.timer.doAfter(0.0, function() mouseClick("left", ptMouse, "cmd"); event_mmb:start(); end)
     end
     return true
 end)
-event_mm:start()
+event_mmb:start()
 
 -- Switch to Google Chrome and press ctrl+. for Tabli
 hs.hotkey.bind({'cmd'}, '.', function()
@@ -207,52 +248,62 @@ end)
 
 
 -- app switching
-k = hs.hotkey.modal.new('', 'F20')
+modal_f20 = hs.hotkey.modal.new('', 'F20')
 
+-- if pressing escape key exit from modal mode
+modal_f20:bind('', 'escape', function() modal_f20:exit(); end)
+-- if clicking with LMB exit from modal mode
+event_lmb = hs.eventtap.new({hs.eventtap.event.types.leftMouseDown}, function(event)
+    event_lmb:stop()
+    hs.timer.doAfter(0.1, function() modal_f20:exit(); end)
+    return false
+end)
+event_lmb:stop()
+
+
+-- Assign keypresses to apps while in modal mode
 for index, value in pairs(key_params) do
-    k:bind('', index, function()
-        launchOrFocus(value)
+    modal_f20:bind('', index, function()
 
-        -- special tasks needed after switching to certain apps
-
-        -- click on the lower bottom corner off the Gmail window to focus and allow hotkeys
-        if (value == "Gmail")
-        then
-            -- click on the Gmail website after focusing it so that they keyboard shortcuts work
-            gmail_frame = hs.window.focusedWindow():frame()
-            point_click = {x=gmail_frame["x"]+15.0, y=gmail_frame["y"]+gmail_frame["h"]-15.0}
+        if type(value) == 'string' then
+            launchOrFocus(value)
+            hs.alert(value)
+        else
+            local app = value['app']
+            launchOrFocus(value['app'], value['win'], value['inclusive'])
+            hs.alert(value['app'])
         end
 
-        -- go to tab with Jira or open a new one
-        if (index == "j")
-        then
-            hs.execute('/usr/local/bin/chrome-cli activate -t $(/usr/local/bin/chrome-cli list links | grep "filter=50263" | grep -o "[0-9][0-9]*\\]" | head -1) || /usr/local/bin/chrome-cli open "https://bugs.indeed.com/issues/?filter=50263"')
+        if value['after'] then
+            value['after']()
         end
 
-        k:exit()
+
+        modal_f20:exit()
     end)
 end
 
-k:bind('', 'escape', function() k:exit(); hs.eventtap.keyStroke({}, "escape") end)
--- k:bind({'ctrl'}, 'down', function() k:exit() end)
 
-
-function k:entered()
-    app = nil
-    mission_control = hs.task.new("/Applications/Mission Control.app/Contents/MacOS/Mission Control", nil)
-    mission_control:start() -- no key will be proccessed until this timer finishes
-    e:start()
-end
-
-function k:exited()
-    if app then
-        hs.alert(app)
-    end
-    e:stop()
-
-    hs.eventtap.keyStroke({}, "escape") -- to exit Mission Control
+function exit_mission_control()
+    -- hs.eventtap.keyStroke({}, "escape") -- to exit Mission Control
     if point_click ~= nil
     then
         hs.timer.doAfter(0.1, function() mouseClick("left", point_click); point_click=nil; end)
     end
+end
+
+
+function modal_f20:entered()
+    all_spaces = spaces.layout()
+    visible_spaces = active_spaces()
+    mission_control = hs.task.new("/Applications/Mission Control.app/Contents/MacOS/Mission Control", nil)
+    -- mission_control:start() -- no key will be proccessed until this timer finishes
+    event_lmb:start()
+end
+
+
+function modal_f20:exited()
+    event_lmb:stop()
+
+    hs.timer.waitUntil(function() return not animating:running(); end, function() exit_mission_control(); end, 0.1)
 end
