9f97a93c9b021683a0a5b83e45adbed85c175c31
jmriego <jmriego@telefonica.net>
2018-07-07 19:32:14 +0100

    zsh: prompt git using vcs_info when possible

--- zsh/prompt/git.zsh
+++ zsh/prompt/git.zsh
@@ -1,46 +1,39 @@
+autoload -Uz vcs_info
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*:*' stagedstr "$PROMPT_ICON_GIT_STAGED"
+zstyle ':vcs_info:git*' formats "%b%c" "%a" "%u"
+zstyle ':vcs_info:git*' actionformats "%b%c" "%a" "%u"
+
+(( ${precmd_functions[(I)vcs_info]} )) || precmd_functions+=(vcs_info)
+
 # Echoes information about Git repository status when inside a Git repository
 git_info() {
 
   # Exit if not inside a Git repository
-  ! git rev-parse --is-inside-work-tree > /dev/null 2>&1 && return
+  [[ -z $vcs_info_msg_0_ ]] && return
 
-  # Git branch/tag, or name-rev if on detached head
-  local GIT_LOCATION=${$(git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD)#(refs/heads/|tags/)}
-
-  local -a DIVERGENCES
-  local -a FLAGS
-
-  local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
-  if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
-    FLAGS+=( "$PROMPT_ICON_GIT_MERGING" )
-  fi
+  local -a GIT_INFO
+  GIT_INFO+=("${PROMPT_ICON_GIT_BRANCH}")
+  GIT_INFO+=("${vcs_info_msg_0_}")
 
   if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
-    FLAGS+=( "$PROMPT_ICON_GIT_UNTRACKED" )
-  fi
-
-  if ! git diff --cached --quiet 2> /dev/null; then
-    FLAGS+=( "$PROMPT_ICON_GIT_STAGED" )
+    GIT_INFO+=("$PROMPT_ICON_GIT_UNTRACKED")
   fi
 
   local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
   if [ "$NUM_AHEAD" -gt 0 ]; then
-    DIVERGENCES+=( "${PROMPT_ICON_GIT_AHEAD//NUM/$NUM_AHEAD}" )
+    GIT_INFO+=( "${PROMPT_ICON_GIT_AHEAD//NUM/$NUM_AHEAD}" )
   fi
 
   local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
   if [ "$NUM_BEHIND" -gt 0 ]; then
-    DIVERGENCES+=( "${PROMPT_ICON_GIT_BEHIND//NUM/$NUM_BEHIND}" )
+    GIT_INFO+=( "${PROMPT_ICON_GIT_BEHIND//NUM/$NUM_BEHIND}" )
   fi
 
   local bgcolor="green"
-  if ! git diff --quiet 2> /dev/null; then
-    bgcolor="yellow"
-  fi
+  [[ -n $vcs_info_msg_2_ ]] && bgcolor="yellow"
 
-  local -a GIT_INFO
-  GIT_INFO+=( "${PROMPT_ICON_GIT_BRANCH}${GIT_LOCATION} " )
-  [[ ${#FLAGS[@]} -ne 0 ]] && GIT_INFO+=( "${(j::)FLAGS}" )
-  [[ ${#DIVERGENCES[@]} -ne 0 ]] && GIT_INFO+=( "${(j::)DIVERGENCES}" )
+  GIT_INFO+=("${vcs_info_msg_1_/merge/${PROMPT_ICON_GIT_MERGING}}")
   return_prompt_section "$bgcolor" "" "${(j::)GIT_INFO}"
 }
