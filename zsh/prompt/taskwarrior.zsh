e358f70ce136be288f69946a4d3ddd188db2e092
Jose <jmriego@telefonica.net>
2018-07-23 14:07:52 +0100

    zsh: prompt taskwarrior fixes

--- zsh/prompt/taskwarrior.zsh
+++ zsh/prompt/taskwarrior.zsh
@@ -5,6 +5,10 @@ taskwarrior_info() {
   local -a TASK_NUMBERS
 
   local inbox="$(task count +in status:pending)"
+  local due_today="$(task due=today status:pending count) "
+  local done_today="$(task end:today status:completed count)"
+  TASK_NUMBERS+=("${PROMPT_ICON_TASK_DONE}${done_today}")
+  [[ $due_today -gt 0 ]] && TASK_NUMBERS+=("${PROMPT_ICON_TASK_URGENT}${due_today}")
   [[ $inbox -gt 0 ]] && TASK_NUMBERS+=("${PROMPT_ICON_TASK_INBOX}${inbox}")
 
   return_prompt_section "white" "" "${(j::)TASK_NUMBERS}"
