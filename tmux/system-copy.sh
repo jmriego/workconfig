1907983e2db5c95e21fd64e232470fb8f8f799af
jmriego <jmriego@telefonica.net>
2019-06-19 18:17:13 +0100

    tmux: copy paste all OS

--- tmux/system-copy.sh
+++ tmux/system-copy.sh
@@ -1,1 +1,23 @@
-xclip -selection clipboard
+#!/usr/bin/env bash
+
+command_exists() {
+    local command="$1"
+    type "$command" >/dev/null 2>&1
+}
+
+# installing reattach-to-user-namespace is recommended on OS X
+if command_exists "pbcopy"; then
+    if command_exists "reattach-to-user-namespace"; then
+        reattach-to-user-namespace pbcopy
+    else
+        pbcopy
+    fi
+elif command_exists "clip.exe"; then # WSL clipboard command
+    clip.exe
+elif command_exists "xsel"; then
+    xsel -i --clipboard
+elif command_exists "xclip"; then
+    xclip -selection clipboard
+elif command_exists "putclip"; then # cygwin clipboard command
+    putclip
+fi
--- tmux/system-paste.sh
+++ tmux/system-paste.sh
@@ -1,1 +1,25 @@
-xclip -selection clipboard -o | tmux load-buffer - && tmux paste-buffer
+#!/usr/bin/env bash
+
+command_exists() {
+    local command="$1"
+    type "$command" >/dev/null 2>&1
+}
+
+paste() {
+    # installing reattach-to-user-namespace is recommended on OS X
+    if command_exists "pbpaste"; then
+        if command_exists "reattach-to-user-namespace"; then
+            reattach-to-user-namespace pbpaste
+        else
+            pbpaste
+        fi
+    elif command_exists "xsel"; then
+        xsel --clipboard
+    elif command_exists "xclip"; then
+        xclip -out -selection clipboard
+    elif command_exists "getclip"; then # cygwin clipboard command
+        getclip
+    fi
+}
+
+paste | tmux load-buffer - && tmux paste-buffer
