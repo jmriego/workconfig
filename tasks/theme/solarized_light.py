1f5a475663c4f7162eb82985404015893c6dcffb
Jose Valenzuela <jvalenzuela@indeed.com>
2020-06-05 17:39:17 +0100

    vit: add config for v2

---
+++ tasks/theme/solarized_light.py
@@ -0,0 +1,15 @@
+theme = [
+    ('list-header', '', '', '', '', ''),
+    ('list-header-column', 'black', 'light gray', '', '#859900', 'light gray'),
+    ('list-header-column-separator', 'black', 'light gray', '', 'black', 'light gray'),
+    ('striped-table-row', 'white', 'dark gray', '', '', 'light gray'),
+    ('reveal focus', 'black', 'dark cyan', 'standout', 'black', '#93a1a1'),
+    ('message status', 'white', 'dark blue', 'standout', 'white', 'dark blue'),
+    ('message error', 'white', 'dark red', 'standout', 'white', 'dark red'),
+    ('status', 'dark magenta', 'black', '', '#93a1a1', ''),
+    ('flash off', 'black', 'black', 'standout', 'black', 'black'),
+    ('flash on', 'white', 'black', 'standout', 'white', 'black'),
+    ('pop_up', 'white', 'black', '', 'white', 'black'),
+    ('button action', 'white', 'light red', '', 'white', 'light red'),
+    ('button cancel', 'black', 'light gray', '', 'black', 'light gray'),
+]
