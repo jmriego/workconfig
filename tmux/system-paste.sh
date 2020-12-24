648d81a260db56bd903d92a85fad7aa186262f35
jmriego <jmriego@telefonica.net>
2020-12-24 15:59:16 +0000

    tmux: fix paste clipboard in wsl

--- tmux/system-paste.sh
+++ tmux/system-paste.sh
@@ -19,6 +19,8 @@ paste() {
         xclip -out -selection clipboard
     elif command_exists "getclip"; then # cygwin clipboard command
         getclip
+    elif command_exists "powershell.exe"; then # cygwin clipboard command
+        powershell.exe -Command get-clipboard | sed 's/\r//g' | perl -pi -e 'chomp if eof'
     fi
 }
 
