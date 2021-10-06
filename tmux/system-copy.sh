ad4d0282917c2d9cf5e9230b37284aa412ed5f18
jmriego <jmriego@telefonica.net>
2021-10-06 21:25:44 +0100

    tmux: improve nested copy

--- tmux/system-copy.sh
+++ tmux/system-copy.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 command_exists() {
     local command="$1"
     type "$command" >/dev/null 2>&1
 }
 
 # installing reattach-to-user-namespace is recommended on OS X
-if command_exists "pbcopy"; then
+if $DIR/is-nested.sh; then
+    printf "\033]52;c;$(base64 | tr -d '\r\n')\a"
+elif command_exists "pbcopy"; then
     if command_exists "reattach-to-user-namespace"; then
         reattach-to-user-namespace pbcopy
     else
