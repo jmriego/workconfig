bd97c13418b201f051564d9844e5c7c4d5a6d916
jvalenzuela <jvalenzuela@indeed.com>
2024-07-11 16:47:43 +0200

    tmux: automatically create session if no next/prev

--- tmux/switch-session.sh
+++ tmux/switch-session.sh
@@ -1,25 +1,37 @@
 #!/usr/bin/env bash
 
-current_session=`tmux display-message -p "#S"`
+function current_session {
+    tmux display-message -p "#S"
+}
+
+function num_sessions {
+    tmux ls | grep -v '^todo:' | wc -l
+}
 
 if [ "$1" == "toggle-todo" ]; then
 
-  (tmux display-message -p '#S' | grep -iqE '^todo$' && tmux switch-client -l) || tmuxinator todo
+  (tmux display-message -p '#S' | grep -iqE '^todo:' && tmux switch-client -l) || tmuxinator todo
 
 elif [ "$1" == "prev" ]; then
 
-  tmux ls \
-    | awk -v s="$current_session" -F: '{ls[NR]=$1} $1==s{i=NR} END {if (i==1) {print ls[NR]} else {print ls[i-1]}}' \
-    | grep -q '^todo$' && tmux switch-client -p 
+  if [[ $(num_sessions) -lt 2 ]]; then
+      tmux new-session -d
+  fi
 
   tmux switch-client -p
+  if [[ "$(current_session)" == "todo" ]]; then
+      tmux switch-client -p
+  fi
 
 else
 
-  tmux ls \
-    | awk -v s="$current_session" -F: '{ls[NR]=$1} $1==s{i=NR} END {if (i==NR) {print ls[1]} else {print ls[i+1]}}' \
-    | grep -q '^todo$' && tmux switch-client -n
+  if [[ $(num_sessions) -lt 2 ]]; then
+      tmux new-session -d
+  fi
 
   tmux switch-client -n
+  if [[ "$(current_session)" == "todo" ]]; then
+      tmux switch-client -n
+  fi
 
 fi
